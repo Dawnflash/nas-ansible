828304acc27708b8f68f3d3f44ae9a4f9ad7fccc
Adam Zahumensky <adamzahumensky@gmail.com>
2021-08-17 02:04:28 +0200

    Parametrize hostname

--- templates/scripts/cert_postrenew.sh
+++ templates/scripts/cert_postrenew.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SDIR=/etc/letsencrypt/live/nas.dawnflash.cz
+SDIR=/etc/letsencrypt/live/{{ hostname }}
 DDIR=/etc/ssl/private
 
 # reload nginx
 service nginx reload
 # sync cert with IPv4 proxy
-scp -q $SDIR/fullchain.pem root@dawnflash.cz:$DDIR/nas.dawnflash.cz.pem
-scp -q $SDIR/privkey.pem root@dawnflash.cz:$DDIR/nas.dawnflash.cz.key
+scp -q $SDIR/fullchain.pem root@dawnflash.cz:$DDIR/{{ hostname }}.pem
+scp -q $SDIR/privkey.pem root@dawnflash.cz:$DDIR/{{ hostname }}.key
 
 ssh root@dawnflash.cz "apache2ctl -k graceful"
