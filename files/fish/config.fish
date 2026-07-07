7c63cb7556f98f4fd17efaeadc4dc79e575b835c
Dawn Zahumenská <dawn@dawnflash.cz>
2026-07-07 18:32:51 +0200

    add fish plugins

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -14,6 +14,16 @@ set -gx DOCKER_API_VERSION 1.52
 
 fish_add_path -gP ~/bin
 
+# pure prompt
+set -U pure_show_jobs true
+set -U pure_color_primary cyan
+set -U pure_show_system_time true
+set -U pure_show_exit_status true
+set -U pure_convert_exit_status_to_signal true
+set -U pure_show_prefix_root_prompt true
+set -U pure_show_subsecond_command_duration true
+set -U pure_threshold_command_duration 1
+
 function diskid
     lsblk -JO | jq -r '.blockdevices[] | (if .hctl then (.hctl | split(":")[0] | tonumber | . + 1) else "-" end) as $bay | "\(.name) = \(.serial):\(.model) (WWN \(.wwn)) (BAY \($bay))"'
 end
